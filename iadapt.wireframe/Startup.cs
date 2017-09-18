8137d572c0bf328ac759cf98940f7a4f83ec7477
Ana Weller <anaeweller@gmail.com>
2017-09-18 13:36:22 +0100

    Add project files.

---
+++ iadapt.wireframe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iadapt.wireframe.Startup))]
+namespace iadapt.wireframe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
